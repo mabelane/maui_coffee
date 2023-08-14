9a2d6cb04bb01c1b0b8e14a67f0ce7d2eef4f64a
mabelane <emmanuelbhuti@gmail.com>
2023-08-14 12:51:55 +0200

    second commit

--- maui_coffee/Services/CoffeeService.cs
+++ maui_coffee/Services/CoffeeService.cs
@@ -24,18 +24,18 @@ namespace maui_coffee.Services
         {
             if (coffeeList?.Count > 0)
                 return coffeeList;
-            var url = "https://raw.githubusercontent.com/mabelane/maui_coffee/main/maui_coffee/Resources/Raw/coffeedata.json";
+            /* var url = "https://raw.githubusercontent.com/mabelane/maui_coffee/main/maui_coffee/Resources/Raw/coffeedata.json";
 
             var response = await HttpClient.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
                 coffeeList = await response.Content.ReadFromJsonAsync<List<Coffee>>();
-            }
+            } */
 
-            /* using var stream = await FileSystem.OpenAppPackageFileAsync("coffeedata.json");
+            using var stream = await FileSystem.OpenAppPackageFileAsync("coffeedata.json");
             using var reader = new StreamReader(stream);
             var content = await reader.ReadToEndAsync();
-            coffeeList = JsonSerializer.Deserialize<List<Coffee>>(content); */
+            coffeeList = JsonSerializer.Deserialize<List<Coffee>>(content); 
             return coffeeList;
         }
     }
